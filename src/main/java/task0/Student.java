923070445c2edd829d349d6cd3dacaeb0ff260b6
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2019-10-18 15:02:14 +0200

    Added class implementation for Professor. Id was not initialized in Student class constructor

--- src/main/java/task0/Student.java
+++ src/main/java/task0/Student.java
@@ -15,6 +15,7 @@ public class Student {
     String surname;
 
     public Student(int id, String name, String surname) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
     }
